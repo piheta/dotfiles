d37ce42faab77383b50451d89ccfae0a18b54b4b
Mateusz Picheta <m@picheta.dev>
2024-11-05 17:17:38 +0100

    Add Brew bundle

--- setup.sh
+++ setup.sh
@@ -1,7 +1,20 @@
-#!/bin/sh
-mkdir -p ~/.config
-ln -s $(pwd)/kitty ~/.config/kitty
-ln -s $(pwd)/nvim ~/.config/nvim
-ln -s $(pwd)/skhd ~/.config/skhd
+# #!/bin/sh
 
-# ln -s $(pwd)/.zshrc ~/.zshrc
+ask() {
+    read -p "$1 [y/n]: " yn
+    case $yn in
+        [Yy]* ) return 0;;  # Yes
+        * ) return 1;;      # No
+    esac
+}
+
+ask "Install Homebrew?" && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+ask "Install packages from the Brewfile?" && brew bundle --file Brewfile
+
+mkdir -p ~/.config
+
+[ ! -e ~/.config/kitty ] && ln -s "$(pwd)/kitty" ~/.config/kitty
+[ ! -e ~/.config/nvim ] && ln -s "$(pwd)/nvim" ~/.config/nvim
+[ ! -e ~/.config/lf ] && ln -s "$(pwd)/lf" ~/.config/lf
+[ ! -e ~/.config/skhd ] && ln -s "$(pwd)/skhd" ~/.config/skhd
+#[ ! -e ~/.zshrc ] && ln -s "$(pwd)/.zshrc" ~/.zshrc
