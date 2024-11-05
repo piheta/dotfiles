016a22f583abb1f097e457c9c92e6ba71f0bf4fb
Mateusz Picheta <m@picheta.dev>
2024-11-05 22:38:09 +0100

    Add python to treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,6 @@ return
 {
 	"nvim-treesitter/nvim-treesitter",
     opts = {
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "go", "javascript", "svelte", "html", "css"},
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "go", "javascript", "svelte", "html", "css", "python"},
     }
 }
