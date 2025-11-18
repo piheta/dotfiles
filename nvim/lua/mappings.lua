82211b0f145bc42cb9bdece4ca17cd21bf972735
piheta <m@picheta.dev>
2025-11-18 13:13:19 +0100

    Add go developement shortcuts

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -19,3 +19,8 @@ map("n", "<C-d>", "<C-d>zz")
 
 -- Go error check
 map("n", "<leader>ee", "oif err != nil {<CR>return err<CR>}<ESC>", { desc = "Insert Go error check" })
+
+-- Go Developement
+map("n", "<leader>gos", ":!swag init<CR>", { desc = "Swag Init" })
+map("n", "<leader>gol", ":!golangci-lint run<CR>", { desc = "Golangci-lint Run" })
+map("n", "<leader>got", ":!go test ./...<CR>", { desc = "Go Test" })
