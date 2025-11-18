a6fd2ffc4af5398ef6101b119162d8a603c8d541
piheta <m@picheta.dev>
2025-11-18 13:34:57 +0100

    Add neogit

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     opts = require "configs.conform",
   },
 
@@ -13,19 +13,45 @@ return {
   },
 
   {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "go", "javascript", "svelte", "html", "css", "python"},
-  	},
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "markdown",
+        "markdown_inline",
+        "go",
+        "javascript",
+        "svelte",
+        "html",
+        "css",
+        "python",
+      },
+    },
   },
 
   {
-     "nvim-tree/nvim-tree.lua",
-     opts = {
-       view = {
-         side = "right",
-       },
-     }
-  }
+    "nvim-tree/nvim-tree.lua",
+    opts = {
+      view = {
+        side = "right",
+      },
+    },
+  },
 
+  {
+    "NeogitOrg/neogit",
+    lazy = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "nvim-telescope/telescope.nvim",
+    },
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+    },
+  },
 }
